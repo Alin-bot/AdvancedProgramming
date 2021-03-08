ab17335737afa5b6323907800b7c5898385a96fa
Vezeteu Alin <57156295+Alin-bot@users.noreply.github.com>
2021-03-08 09:58:05 +0200

    Add files via upload

---
+++ Lab4Compulsory/src/com/compulsory/Student.java
@@ -0,0 +1,27 @@
+package com.compulsory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Student {
+
+    String studentName;
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public Student(String studentName) {
+        this.studentName = studentName;
+    }
+
+    @Override
+    public String toString() {
+        return studentName;
+    }
+}
