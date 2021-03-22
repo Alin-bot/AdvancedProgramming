b075f65f1fa69fc511f48e7bed9b318ec9a8c9ad
Vezeteu Alin <alin.vezeteu@yahoo.ro>
2021-03-22 09:42:22 +0200

    compulsory

---
+++ Lab6Compulsory/src/com/compulsory/ConfigPanel.java
@@ -0,0 +1,28 @@
+package com.compulsory;
+
+import javax.swing.*;
+import java.util.Random;
+
+public class ConfigPanel extends JPanel {
+    final MainFrame frame;
+    JLabel sidesLabel; // we’re drawing regular polygons
+    JSpinner sidesField; // number of sides
+    JComboBox colorCombo; // the color of the shape
+
+    public ConfigPanel(MainFrame frame) {
+        this.frame = frame;
+        init();
+    }
+    private void init() {
+        //create the label and the spinner
+        sidesLabel = new JLabel("Number of sides:");
+        sidesField = new JSpinner(new SpinnerNumberModel(0, 0, 100, 1));
+        sidesField.setValue(6); //default number of sides
+        //sidesField = new JComboBox<>();
+        //create the colorCombo, containing the values: Random and Black
+ //...TODO
+        add(sidesLabel); //JPanel uses FlowLayout by default
+        add(sidesField);
+        //add(colorCombo);
+    }
+}
